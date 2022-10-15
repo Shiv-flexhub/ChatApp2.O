44e3b3a91fc681d5b18abfdf0b931611d923791d
shivflex <shivachaubey01@gmail.com>
2022-10-15 21:08:19 +0530

    updated router.js and deleted package-lock.json

--- server/router.js
+++ server/router.js
@@ -1,8 +1,5 @@
 const express = require("express")
 const router = express.Router();
 
-router.get('/',(req,res)=>{
-    res.send({ response: "Server is up and running." }).status(200);
-})
 
 module.exports = router;
